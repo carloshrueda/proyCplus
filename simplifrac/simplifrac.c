0dd4741ad099e3d4237138693316f7fb923b497b
carloshrueda <carloshrueda@gmail.com>
2017-09-27 21:04:52 -0500

    actualizando proyecto desde Cevelop C++ IDE

--- simplifrac/simplifrac.c
+++ simplifrac/simplifrac.c
@@ -4,46 +4,45 @@
 
 #include "tadfrac.h"
 
-void leerNumeros(int *num, int *den) {
+void leerNumeros(int *num, int *den)
+{
 	int n;
 	char msgerror[255];
 
 	printf("Numerador: ");
-    n=scanf("%d", num);
-    if (n != 1)
-    {
-        if (errno != 0)
-        {
-       		perror(msgerror);
-       		puts( msgerror );
-       		printf("\n");
-       		exit(1);
-       	}
-       	else
-       	{
-           printf("Error. No se pudo leer el numerador.\n");
-           exit(1);
-       	}
-    }
-    printf("Denominador: ");
-    n=scanf("%d", den);
-    if (n != 1)
-    {
-        if (errno != 0)
-        {
-       		perror(msgerror);
-       		puts( msgerror );
-       		printf("\n\n");
-       		exit(1);
-       	}
-       	else
-       	{
-           printf("Error. No se pudo leer el denominador.\n\n");
-           exit(1);
-       	}
-    }
-
-
+	n = scanf("%d", num);
+	if (n != 1)
+	{
+		if (errno != 0)
+		{
+			perror(msgerror);
+			puts(msgerror);
+			printf("\n");
+			exit(1);
+		}
+		else
+		{
+			printf("Error. No se pudo leer el numerador.\n");
+			exit(1);
+		}
+	}
+	printf("Denominador: ");
+	n = scanf("%d", den);
+	if (n != 1)
+	{
+		if (errno != 0)
+		{
+			perror(msgerror);
+			puts(msgerror);
+			printf("\n\n");
+			exit(1);
+		}
+		else
+		{
+			printf("Error. No se pudo leer el denominador.\n\n");
+			exit(1);
+		}
+	}
 
 }
 
@@ -51,12 +50,10 @@ int main(void)
 {
 	int num, den;
 
+	leerNumeros(&num, &den);
 
-    leerNumeros(&num, &den);
+	simplificarfrac(&num, &den);
 
-    simplificarfrac(&num, &den);
-
-
-    return 0;
+	return 0;
 
 }
